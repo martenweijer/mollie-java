2d7ca4dd8751d63f37f36e3e255914a884689916
martenweijer <martenweijer@gmail.com>
2021-03-08 20:06:37 +0100

    Add documentation (#7)

--- library/src/main/java/com/electronics/models/Amount.java
+++ library/src/main/java/com/electronics/models/Amount.java
@@ -3,4 +3,12 @@ package com.electronics.models;
 public class Amount {
     public String currency;
     public String value;
+
+    public Amount() {
+    }
+
+    public Amount(String currency, String value) {
+        this.currency = currency;
+        this.value = value;
+    }
 }
--- library/src/main/java/com/electronics/models/CreatePayment.java
+++ library/src/main/java/com/electronics/models/CreatePayment.java
@@ -4,4 +4,13 @@ public class CreatePayment {
     public String description;
     public Amount amount;
     public String redirectUrl;
+
+    public CreatePayment() {
+    }
+
+    public CreatePayment(String description, Amount amount, String redirectUrl) {
+        this.description = description;
+        this.amount = amount;
+        this.redirectUrl = redirectUrl;
+    }
 }
