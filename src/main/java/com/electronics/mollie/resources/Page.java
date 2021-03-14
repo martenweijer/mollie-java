23c88ed6d228fa94fe01eac6c3e5109d0bb99d4a
martenweijer <martenweijer@gmail.com>
2021-03-14 18:10:33 +0100

    Payment list (#12)

---
+++ src/main/java/com/electronics/mollie/resources/Page.java
@@ -0,0 +1,39 @@
+package com.electronics.mollie.resources;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Page<T> {
+    @JsonProperty("_links")
+    private Links links;
+    private int count;
+    private Embedded<T> _embedded;
+
+    public Links getLinks() {
+        return links;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public List<T> getItems() {
+        if (_embedded == null) {
+            return new ArrayList<>();
+        }
+
+        return _embedded.getItems();
+    }
+
+    private static class Embedded<T> {
+        @JsonAlias({"payments"})
+        private List<T> items;
+
+        public List<T> getItems() {
+            return items;
+        }
+    }
+}
